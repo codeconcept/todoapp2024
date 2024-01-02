7749378eacac0a1e636b76cd39816c79fc1a757f
Code Concept <contact@codeconcept.com>
2024-01-02 14:56:15 +0100

    Reactive Form with a standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoapp2024';
+  taskForm = new FormGroup({
+    taskName: new FormControl(''),
+    taskDate: new FormControl(''),
+  });
+
+  addTask(evt: Event): void {
+    evt.preventDefault();
+    console.log(this.taskForm.value);
+    this.taskForm.reset();
+  }
 }
