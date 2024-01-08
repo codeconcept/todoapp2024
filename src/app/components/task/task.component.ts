7f70629291d5328cc4861f792e0cd1f95274a680
Code Concept <contact@codeconcept.com>
2024-01-08 13:54:15 +0100

    Date pipe + local fr-FR

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import Task from '../../interfaces/task';
+// https://angular.io/api/common/DatePipe
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      task {{ task.taskName }} works!   
-    </p>
+   <div class="task">
+    <h3>{{ task.taskName }}</h3>
+    <p>A faire avant le : {{ task.taskDate | date:'EEEE dd MMMM YYYY':'':'fr-FR' }}</p>
+   </div>
   `,
   styleUrl: './task.component.css'
 })
